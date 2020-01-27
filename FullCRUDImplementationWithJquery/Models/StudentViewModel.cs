028352366f6e60a1fcc0d1c927f6a91ae1844246
Tofael23 <t.a.cse96@gmail.com>
2020-01-28 01:25:55 +0600

    Create Date 03/07/2018

---
+++ FullCRUDImplementationWithJquery/Models/StudentViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FullCRUDImplementationWithJquery.Models
+{
+    public class StudentViewModel
+    {
+        
+      
+        public string Password { get; set; }
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+        public string Email { get; set; }
+        public Nullable<bool> IsDeleted { get; set; }
+        public Nullable<int> DepartmentId { get; set; }
+        public string DepartmentName { get; set; }
+    }
+}
